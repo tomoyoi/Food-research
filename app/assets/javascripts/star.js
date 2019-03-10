c9ab27b317f2a0dc23c0e989fa43492621fbb023
Tomoyo Ito <tmyooo73@gmail.com>
2019-03-10 21:37:14 +0900

    add ratings function

--- app/assets/javascripts/star.js
+++ app/assets/javascripts/star.js
@@ -1,5 +1,12 @@
 $(function(){
   $('#star').raty({
+    size: 70,
+    starOff:  '/assets/star-off.png',
+    starOn: '/assets/star-on.png',
+    starHalf: '/assets/star-half.png',
+    scoreName: 'review[star]',
+  });
+  $('.star').raty({
     size: 36,
     starOff:  '/assets/star-off.png',
     starOn: '/assets/star-on.png',
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,2 @@
 class Restaurant < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
 end
