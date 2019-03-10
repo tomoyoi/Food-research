ff3bcb26b8cf7c1184d9c4170435e28e4f9e10d5
Tomoyo Ito <tmyooo73@gmail.com>
2019-03-10 13:19:34 +0900

    Add user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get "restaurants/index"
-  resources :users, only: [:edit, :update]
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
